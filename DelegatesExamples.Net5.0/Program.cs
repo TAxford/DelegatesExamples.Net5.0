d7987b1e877618c9286a70a25b5c011c0ada1600
Timothy Axford <t.axford2@yahoo.com>
2023-04-10 09:09:25 -0400

    a method called Filter that takes a string then return whether the string s contains the letter 'i' the Contains method will return a bool which will return as well

--- DelegatesExamples.Net5.0/Program.cs
+++ DelegatesExamples.Net5.0/Program.cs
@@ -9,6 +9,15 @@ namespace DelegatesExamples.Net5._0
         {
             //list of names
             List<string> names = new List<string>() { "Aiden", "Sif", "Walter", "Anatoli" };
+            names.RemoveAll
+        }
+
+        //a method called Filter that takes a string
+        static bool Filter(string s) 
+        {
+            //return whether the string s contains the letter 'i'
+            //the Contains method will return a bool which will return as well
+            return s.Contains("i");
         }
     }
 }
